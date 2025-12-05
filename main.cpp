1225a48d85d9bbda0e0565a124616c1b2991fa28
ChrisRM212 <christopher.rivera41@upr.edu>
2025-12-05 17:47:27 -0400

    Main comentado
    Added comments for better code clarity.

--- main.cpp
+++ main.cpp
@@ -4,17 +4,25 @@
 using namespace std;
 
 int main() {
+    //Se crea motor de búsqueda
     SearchEngine engine;
-#
+    
+    // Directorio con archivos
     string directory = "data/movies";
+
+    // Lee archivos y construir índice
     engine.readFile(directory);
     engine.buildIndex();
 
     string query;
+
+    // Permitir múltiples búsquedas
     while (true) {
         cout << "Ingrese consulta (1 o 2 palabras, 'exit' para salir): ";
         getline(std::cin, query);
         if (query == "exit") break;
+        
+        //Busca y muestra resultados
         engine.search(query);
     }
 
